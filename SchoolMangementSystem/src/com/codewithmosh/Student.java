86a5861f8288cc4f242509c0f5c03f5b0d148eaf
shorouk <shorouk.talat23@gmail.com>
2020-07-29 05:21:46 +0200

    [ Feateure ] Setting Salary Of Teacher And Paying Of Student

---
+++ SchoolMangementSystem/src/com/codewithmosh/Student.java
@@ -0,0 +1,32 @@
+package com.codewithmosh;
+
+public class Student {
+
+
+    private String name;
+    private int id;
+    private int paidFees;
+    private int totalFees;
+
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+        paidFees=0;
+        totalFees=3000;
+    }
+
+    public void payFees(int paidFees) {
+        if(paidFees<totalFees)
+        this.paidFees += paidFees;
+
+
+    }
+
+    public int remainingFees() {
+       return totalFees - paidFees;
+    }
+
+
+
+}
---
+++ SchoolMangementSystem/src/com/codewithmosh/Teacher.java
@@ -0,0 +1,29 @@
+package com.codewithmosh;
+
+public class Teacher {
+
+    private String name;
+    private int id;
+    private int salary;
+    private int earnedmoney;
+
+    public Teacher(String name, int id,int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        earnedmoney=0;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+    public void recieveMoney(int money) {
+        earnedmoney+=money;
+
+    }
+
+
+
+}
