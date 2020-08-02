3d7bcd7b2d1bf91f8d3219f45219ffa375d497d3
shorouk <shorouk.talat23@gmail.com>
2020-08-02 23:26:17 +0200

    [ Feature ] User To Enter Num Of Students To Database

--- StudentMangementSys/src/com/codewithmosh/Main.java
+++ StudentMangementSys/src/com/codewithmosh/Main.java
@@ -1,8 +1,20 @@
 package com.codewithmosh;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Student s1=new Student();
+        System.out.println("enter the number of tudents added to the database");
+        Scanner scan=new Scanner(System.in);
+         var num=scan.nextInt();
+
+        var students=new Student[num];
+        for (int i=0;i<num;i++ ){
+            students[i]=new Student();
+        }
+
+        System.out.println(students[0].showInfo());
     }
 }
